9af22a07e61568cd316d31801cd8e077b619f1e8
pavithraax <pavithraa2007@gmail.com>
2026-05-27 23:19:38 +0530

    Improved firmware architecture and dashboard structure

--- firmware/esp32_main.ino
+++ firmware/esp32_main.ino
@@ -1,19 +1,41 @@
+#include <WiFi.h>
+
 #define PIR_PIN 13
 #define RELAY_PIN 26
 
+const char* WIFI_NAME = "YOUR_WIFI_NAME";
+const char* WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
+
 bool motionDetected = false;
 
-void setup() {
+void initializePins() {
 
     pinMode(PIR_PIN, INPUT);
     pinMode(RELAY_PIN, OUTPUT);
 
-    Serial.begin(115200);
-
-    Serial.println("SmartRoom-X Initializing...");
+    digitalWrite(RELAY_PIN, LOW);
 }
 
-void loop() {
+void connectWiFi() {
+
+    Serial.println();
+    Serial.println("Connecting to Wi-Fi...");
+
+    WiFi.begin(WIFI_NAME, WIFI_PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED) {
+
+        delay(1000);
+        Serial.println("Connecting...");
+    }
+
+    Serial.println();
+    Serial.println("Wi-Fi Connected");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void handleMotionDetection() {
 
     motionDetected = digitalRead(PIR_PIN);
 
@@ -27,6 +49,22 @@ void loop() {
         Serial.println("No Motion");
         digitalWrite(RELAY_PIN, LOW);
     }
+}
+
+void setup() {
+
+    Serial.begin(115200);
+
+    Serial.println("Starting SmartRoom-X");
+
+    initializePins();
+
+    connectWiFi();
+}
+
+void loop() {
+
+    handleMotionDetection();
 
     delay(1000);
 }
